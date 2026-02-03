17bdf066d970f579cdec14081f41afd71d5dadef
jensmorten <jens.morten.nilsen@gmail.com>
2026-02-03 09:48:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ df_visning = df_visning.rename(columns={
 
 #df_visning=df_visning[['Veg_ID', 'Art', 'ÅDT (Årsdøgntrafikk)', 'Lengde (m)','frekvens','lenke']].copy()
 df_visning = df_visning[
-    ['Veg_ID', 'Art', 'ÅDT (Årsdøgntrafikk)', 'Lengde (m)', metric_label, 'lenke']
+    ['Veg_ID', 'Art', 'ÅDT (Årsdøgntrafikk)', 'Lengde (m)', 'antall_kollisjoner', metric_label, 'lenke']
 ].copy()
 
 # -----------------------------
