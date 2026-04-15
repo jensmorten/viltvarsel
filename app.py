944ae7f484943356f540be58a657239c523c79da
jensmorten <jens.morten.nilsen@gmail.com>
2026-04-15 21:12:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -146,22 +146,23 @@ try:# metadata.json
 
 except:
     METADATA = {
-        'sist_oppdatert': "2025-01-01 12:00",
-        'første_kollisjon': "2025-01-01 12:00",
-        'siste_kollisjon': "2025-01-31 12:00"
+        'sist_oppdatert': "31.01.2026 kl. 12:00",
+        'første_kollisjon': "01.01.2025 kl. 12:00",
+        'siste_kollisjon': "31.02.2026 kl. 12:00"
     }
 
 
-#def _fmt_dato(s):
-#    return pd.to_datetime(s).strftime("%d.%m.%y kl. %H:%M")
+def _fmt_dato(s):
+    return pd.to_datetime(s).strftime("%d.%m.%y kl. %H:%M")
 
-##sist_oppdatert  = _fmt_dato(METADATA['sist_oppdatert'])
-##første_kollisjon = _fmt_dato(METADATA['første_kollisjon'])
-##siste_kollisjon  = _fmt_dato(METADATA['siste_kollisjon'])
-
-sist_oppdatert =  "2025-01-01 12:00",
-første_kollisjon= "2025-01-01 12:00",
-siste_kollisjon= "2025-01-31 12:00"
+try:
+    sist_oppdatert  = _fmt_dato(METADATA['sist_oppdatert'])
+    første_kollisjon = _fmt_dato(METADATA['første_kollisjon'])
+    siste_kollisjon  = _fmt_dato(METADATA['siste_kollisjon'])
+except:
+    sist_oppdatert =  "2025-01-01 12:00",
+    første_kollisjon= "2025-01-01 12:00",
+    siste_kollisjon= "2025-01-31 12:00"
 
 # --------------------------------------------------
 # Sidebar – brukarval
