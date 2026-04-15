d80096db1c8f85d05a6831ed03501d1f6222eea7
jensmorten <jens.morten.nilsen@gmail.com>
2026-04-15 21:07:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -146,8 +146,8 @@ try:# metadata.json
 
 except:
     METADATA = {
-        'sist_oppdatert': '2025-01-01 12:00",
-        'første_kollisjon': 2025-01-01 12:00",
+        'sist_oppdatert': "2025-01-01 12:00",
+        'første_kollisjon': "2025-01-01 12:00",
         'siste_kollisjon': "2025-01-31 12:00"
     }
 
